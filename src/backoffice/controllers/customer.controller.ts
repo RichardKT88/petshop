f70d20fb1cb89b48a0a6c93f896bcae6834b5684
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2023-03-07 16:18:58 -0300

    Intro - Postman

---
+++ src/backoffice/controllers/customer.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+
+@Controller('v1/customers')
+export class CustomerController {
+  @Get()
+  get() {
+    return 'Obter os clientes';
+  }
+  @Post()
+  post() {
+    return 'Criar um cliente';
+  }
+  @Put()
+  put() {
+    return 'Atualizar um cliente';
+  }
+  @Delete()
+  delete() {
+    return 'Remover um cliente';
+  }
+}
